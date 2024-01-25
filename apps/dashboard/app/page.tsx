ada2009a604d75b5034283b29596d785e38c0181
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-01-25 15:06:48 +0530

    feat(setup): upgrade next@14.1.0 and add tailwindcss

--- apps/dashboard/app/page.tsx
+++ apps/dashboard/app/page.tsx
@@ -1,5 +1,10 @@
-export default function Page() {
+export default async function Page() {
+  console.log(process.env.DATABASE_URL)
   return (
-    <div>Page</div>
+    <div className="text-white h-full flex items-center justify-center bg-[#121212]">
+      <button className="bg-emerald-500 text-white rounded-md py-2.5 px-6">
+        Hello from Dashboard
+      </button>
+    </div>
   );
 }
---
+++ apps/www/tailwind.config.ts
@@ -0,0 +1,6 @@
+import sharedConfig from "@repo/tailwind/tailwind.config.ts";
+import type { Config } from "tailwindcss";
+
+const config: Config = sharedConfig;
+
+export default config;
